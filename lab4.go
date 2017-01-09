330b9f5a1b368ac96913be19c4ed4fc85593bf32
KirillMityukevich <mr.mityukevich@yandex.ru>
2017-01-09 23:43:24 +0700

    С опозданием
    Прошу прощения

---
+++ lab4.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+	data      string
+}
+
+var deep int = 0
+
+	ch1 <- t
+
+}
+
+
+		deep++
+		go get()
+	} else {
+	}
+
+}
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	N := rand.Intn(100)
+
+
+	go get()
+
+}
